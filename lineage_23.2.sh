be570df59bd66cfea32d4dd5c44caedc68fd009f
akram-6 <akramplays09@gmail.com>
2026-07-03 20:20:28 +0600

    .

---
+++ lineage_23.2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone  https://github.com/akram-arc/local_manifests.git .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=Akram 
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+breakfast RM6785
+
+# Build
+brunch RM6785
+
