05ccdda817e76c424229b0b4135c6620315b70ef
akram-6 <akramplays09@gmail.com>
2026-07-04 20:55:42 +0600

    Local TimeZone added

--- lineage_23.2.sh
+++ lineage_23.2.sh
@@ -2,6 +2,12 @@
 
 rm -rf .repo/local_manifests/
 
+
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
+
+
 # repo init rom
 repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 echo "=================="
