6c9f1f442ed10e2ac46adb0dce7931d149ad6c00
akram-arc <194501848+akram-arc@users.noreply.github.com>
2026-08-16 12:38:11 +0600

    Island: fix

--- lineage_23.2.sh
+++ lineage_23.2.sh
@@ -88,7 +88,7 @@ echo ">>> Cherry picking base patches..."
 cd frameworks/base
 git remote add akram-arc https://github.com/akram-arc/android_frameworks_base.git 2>/dev/null || true
 git fetch akram-arc
-git cherry-pick bf1f04383176d6cf9fd1cef19ef31663ab8ce1d9 850581e22afa47e3c958e4cb637374aceb789154 7c2acb070646d0cc6572133cd2862dc49d984b4e a16fb0d01d4bd6b8399f0804ffcda7b6e09e270e 8c092990466e10b0feb0ea6e5ae0f79cf3460375 2e284640f07ca4ad83b9a88766ad02788dfd366d 7370cf80ff25f46c978019345e25c3be79b39945 9b066176d6f007b5fcac0fc497364ac3af3039ea fd447c4857bcf60790e7218265722b207f877163 dcb717372ab930d01eacb01ddbf68226acbb0bb5
+git cherry-pick 73fa97a3518017125f126fc67040812323bfa335 792a043e670c731cea03ed1696e6149a81749b11 3f9b219c6fead188c45b80d2e383df38efb4d7fa 8c081f5968186363c3ab58023f702469dcbc8d77 447f0a62546c3f22fe8e8eb1d1e55b6ee08d9ac5
 cd ../..
 
 # sdk patches
@@ -96,7 +96,7 @@ echo ">>> Cherry picking sdk patches..."
 cd lineage-sdk
 git remote add akram-arc https://github.com/akram-arc/android_lineage-sdk.git 2>/dev/null || true
 git fetch akram-arc
-git cherry-pick f9329886c332783d191c6ac08f5e3f8fd849fb62 b2ebaf25b11156f43b689fef173e9f95d117d38d 8233504dfc613a0041c57925998f3bc03ab74df6 35e36fcb152d7fbc949c03fc9fd6dc3fc974c956 36bd1cc390a0321796d8237e071fb0def8b8d3d5 0de890eda8570aa4b69e0604878896b96796d788
+git cherry-pick 56816dc1cf7a70e3e3d24bc0ca1df1eb0a13c03c 71b01f0fa9c3b412f22804d9942f53e9a80a40d8 c74fbcfb06c74910e3a767ce91c9bf300cf695fc 9145b247e9b0050375d961468ce433d697c3d550 fe58f079419a4c00677c4fc64debb0316b1c2036
 cd ..
 
 # LineageParts patches
@@ -104,7 +104,7 @@ echo ">>> Cherry picking LineageParts patches..."
 cd packages/apps/LineageParts
 git remote add akram-arc https://github.com/akram-arc/android_packages_apps_LineageParts.git 2>/dev/null || true
 git fetch akram-arc
-git cherry-pick 74dcee8186a32d8a801a04ad0f0b45ab1421b11a c891e4c101c65e302d6b6545bc5fa75f0e6157fe 9374e946053953fb65ab88638597c9518fac2453 9823a38572d62ce139c9f99fc23f48ba16054234 04b7601e011ef17761d5986355ce653e480a5c60 94abfc10435afe483bf948e88faf62d902228064 ebd91b76a53bf0313e515a834c09228153fba91a 35b8fee7fadeda7a1c4a8d46f13287b3e870679a 36e139c01d19c05d8d65d9f022e84b8216651e9a 8e30960c2ac8b891bbafd2a2b2bd60e09fab8782 a0d6aa9035bc47ee6176b8bc0763e9fb038f2e2d
+git cherry-pick 9f5d2711d47b250cb8ad6d3a37a33ae5b6c89559 ec1037618a79f40ef0df12e4775b1e996edcfd40 c86a2cc41b27b9011b637d3cdf72576012b133f9 27016f8d69f6605876c4824e07af93c77f2697f4
 cd ../../..
 
 echo "====================="
