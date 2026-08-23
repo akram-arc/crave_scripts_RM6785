a9b3c49f8fc8efc91f14b8074385e1160fb7a876
akram-arc <194501848+akram-arc@users.noreply.github.com>
2026-08-23 15:19:30 +0600

    RM6785: local time

--- lineage_23.2.sh
+++ lineage_23.2.sh
@@ -9,6 +9,10 @@ rm -rf vendor/realme/RM6785
 rm -rf device/mediatek/sepolicy_vndr
 rm -rf hardware/mediatek
 
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
+
 # repo init rom
 repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 echo "=================="
