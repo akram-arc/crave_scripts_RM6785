4f615db4256f11c63414c3d1632ee256b74b50dc
akram-arc <194501848+akram-arc@users.noreply.github.com>
2026-08-23 16:07:10 +0600

    Crave: clean up

--- lineage_23.2.sh
+++ lineage_23.2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests/
-rm -rf .repo/local_manifests/
 rm -rf vendor/gms
 rm -rf device/realme
 rm -rf kernel/realme
@@ -14,7 +13,7 @@ sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
 
 # repo init rom
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
