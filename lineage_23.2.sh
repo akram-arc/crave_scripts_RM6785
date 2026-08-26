df1baead88f5cb6f1fde43686514ad341e7585a3
akram-arc <194501848+akram-arc@users.noreply.github.com>
2026-08-26 21:12:45 +0600

    just testing nothing else

--- lineage_23.2.sh
+++ lineage_23.2.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests/
+rm -rf vendor/gms
+rm -rf device/realme
+rm -rf kernel/realme
+rm -rf vendor/realme
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf hardware/mediatek
+
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
+
 
 # repo init rom
 repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
